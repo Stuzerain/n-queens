ddf262698de4e5dc2e7277a49ae3f46539c6fd89
Stu Hanford <shanford3@gmail.com>
2020-08-27 19:13:56 -0700

    Implement partial solutions for find and count nQueens

--- src/solvers.js
+++ src/solvers.js
@@ -23,7 +23,6 @@ new Board ([filled matrix])
 [0, 0, 1, 0],
 [0, 0, 0, 1]
 ]
-
 */
 
 window.findNRooksSolution = function (n) {
@@ -42,7 +41,7 @@ window.findNRooksSolution = function (n) {
   }
 
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
@@ -59,46 +58,126 @@ window.countNRooksSolutions = function (n) {
     return n * factorial(n - 1);
   };
   let solution = factorial(n);
-  console.log('Number of solutions for ' + n + ' rooks:', solution);
+  // console.log('Number of solutions for ' + n + ' rooks:', solution);
   return solution;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function (n) {
-  var solution = new Board({ n: n }); //fixme
-
-
-  if (!solution.hasAnyQueensConflicts() && solution.countPieces() === n) {
-    console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-    return solution.rows();
+  if (n === 1) {
+    // debugger;
   }
+  let solution = new Board({ n: n });
 
-  let checkBranches = function (board) {
-    /*
-
-     know we need to toggle pieces
-     when we toggle pieces they cannot have conflicts
-     toggle a piece in first availble spot then next ++
-     if no available space "return"
-
+  // if (n === 0 || n === 2 || n === 3) {
+  //   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution.rows()));
+  //   return solution.rows();
+  // }
+  let sample;
 
 
+  let checkBranches = function (rowNum) {
+    if (!solution.hasAnyQueensConflicts() && solution.countPieces() === n) {
+      console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution.rows()));
+      // debugger;
+      // console.log(matrix)
+      // debugger;
+      // return board.rows();
+      // console.log(solution)
+      sample = _.map(solution.rows(), function (row) { return row.slice(); });
+      console.log('sample insample');
+      return;
+    }
+    // if location is undefined
+    //start at 0,0
 
-    */
+    //iterate over the board rows
+    for (let i = 0; i < n; i++) {
+      //toggle piece at first column/row index
+      solution.togglePiece(rowNum, i);
+      //if no conflict
+      if (!solution.hasAnyQueensConflicts()) {
+        checkBranches(rowNum + 1);
+        // solution.togglePiece(rowNum, i);
+        //otherwise
+      } else if (solution.hasAnyQueensConflicts()) {
+        // toggle same piece off and return
+        solution.togglePiece(rowNum, i);
+      }
+      solution.togglePiece(rowNum, i);
+      // checkBranches(rowNum);
+    }
   };
 
+  //  know we need to toggle pieces
+  //  when we toggle pieces they cannot have conflicts
+  //  toggle a piece in first availble spot then next ++
+  //  if no available space "return"
+
+  checkBranches(0);
+  // return solution;
+  console.log('sample at the end', sample);
+  return sample;
 
 
-  return checkBranches(solution.rows());
 
 };
+
+
+
 
 // https://app.ziteboard.com/team/40b21dc1-98f9-457e-aca9-dd11dc2d17d0
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function (n) {
-  var solutionCount = undefined; //fixme
+  let solution = new Board({ n: n });
+  let count = 0;
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+  // if (n === 0 || n === 2 || n === 3) {
+  //   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution.rows()));
+  //   return solution.rows();
+  // }
+
+
+
+  let checkBranches = function (rowNum) {
+    if (!solution.hasAnyQueensConflicts() && solution.countPieces() === n) {
+      // console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution.rows()));
+      // debugger;
+      // console.log(matrix)
+      // debugger;
+      // return board.rows();
+      // console.log(solution)
+      count++;
+    }
+    // if location is undefined
+    //start at 0,0
+
+    //iterate over the board rows
+    for (let i = 0; i < n; i++) {
+      //toggle piece at first column/row index
+      solution.togglePiece(rowNum, i);
+      //if no conflict
+      if (!solution.hasAnyQueensConflicts()) {
+        checkBranches(rowNum + 1);
+        // solution.togglePiece(rowNum, i);
+        //otherwise
+      } else if (solution.hasAnyQueensConflicts()) {
+        // toggle same piece off and return
+        solution.togglePiece(rowNum, i);
+
+      }
+      solution.togglePiece(rowNum, i);
+      // checkBranches(rowNum);
+    }
+  };
+
+  //  know we need to toggle pieces
+  //  when we toggle pieces they cannot have conflicts
+  //  toggle a piece in first availble spot then next ++
+  //  if no available space "return"
+
+  checkBranches(0);
+  // return solution;
+  return count;
 };
